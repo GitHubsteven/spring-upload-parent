e9404e9441300efa3c7e85977e44d49d99d6d90e
rongbin.xie <rongbin.xie@voyageone.cn>
2020-03-16 15:29:49 +0800

    first commit

---
+++ src/test/java/pers/asa/springuploadparent/SpringUploadParentApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.asa.springuploadparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUploadParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
